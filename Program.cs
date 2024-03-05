a6f0c62c6f92acd4ab9345048074510c8a99a868
Ddusya <Viktor.khandus04@gmail.com>
2024-03-05 16:44:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("РР");
+            /*Console.WriteLine("Введите значение x:");
+            double x = double.Parse(Console.ReadLine());
+
+            double result = 8 * Math.Sin(2 * x) - Math.Cos(2 * x);
+
+            Console.WriteLine("Результат вычисления функции: {result});*/
+            Console.ReadKey();
+        }
+    }
+}
